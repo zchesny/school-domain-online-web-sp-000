9d89f3742aebe57309983bc2ca408bb934cfed42
Zoey Chesny <z.chesny@gmail.com>
2020-04-14 00:21:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
 # code here!
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    roster.key?(grade) ? @roster[grade] << name : @roster[grade] = [name]
+  end
+
+end
